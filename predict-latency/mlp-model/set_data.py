abcc5fa3e984cca93ba1f0e7db6b6eede5754fb8
meowpunch <jinhoonbae@gmail.com>
2020-03-08 16:12:03 +0900

    train-denorm

--- predict-latency/mlp-model/set_data.py
+++ predict-latency/mlp-model/set_data.py
@@ -12,6 +12,8 @@ def set_data(filename): # return type : ndarray
 
     X_data, Y_data, Y_min, Y_max, Y_mean, Y_std = preprocess(X_data, Y_data) #standarize + normalize
     Y_data = torch.from_numpy(Y_data).view(data.shape[0],1)
+    
+    # print(X_data, Y_data)
 
     # separate train data and test data & convert to tensor
     train_ratio = 0.8
@@ -20,6 +22,7 @@ def set_data(filename): # return type : ndarray
     X_test = torch.from_numpy(X_data[ratio:,:])
     Y_train = Y_data[:ratio,]
     Y_test = Y_data[ratio:,]
+ 
 
 #     print("----------tensor ver.---------------")
 #     print("----> X_train\n{}\n---->Y_train\n{}\n---->X_test\n{}\n---->Y_test\n{}".format(X_train, Y_train, X_test, Y_test))
@@ -34,8 +37,8 @@ def preprocess(X_data, Y_data): # return type : ndarray
         max = X_data[:, i].max()
         for j in range(X_data[:,i].shape[0]):
             e = X_data[:, i][j]
-            X_data[:, i][j] = (e - min) / max
-#     print("----------normalize--------------\n", "X_Data: ",X_data)
+            X_data[:, i][j] = (e - min) / (max - min)
+    print("----------normalize--------------\n", "X_Data: ",X_data)
 
     # standarize (if needed)
     for i in range(X_data.shape[1]):
@@ -48,17 +51,22 @@ def preprocess(X_data, Y_data): # return type : ndarray
 #     print("----------standarize--------------\n", "X_Data: ",X_data)
 
     # for Y
-    y_std = Y_data.std()
-    y_mean = Y_data.mean()
+
     y_min = Y_data.min()
     y_max = Y_data.max()
     
+    # print(Y_data)
     for i in range(Y_data.shape[0]):
         e = Y_data[i]
-        Y_data[i] = (e - y_min) / y_max
-
+        Y_data[i] = (e - y_min) / (y_max - y_min)
+    # print(Y_data)
+    
+    y_std = Y_data.std()
+    y_mean = Y_data.mean()
     for i in range(Y_data.shape[0]):
         e = Y_data[i]
         Y_data[i] = (e - y_mean) / y_std
+    print(Y_data)
 
     return X_data, Y_data, y_min, y_max, y_mean, y_std
+
