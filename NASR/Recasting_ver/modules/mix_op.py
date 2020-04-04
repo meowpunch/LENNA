c82329e3386b2c0b75edd4e1902f699b82734e4f
meowpunch <jinhoonbae@gmail.com>
2020-04-04 10:47:54 +0900

    MEOW: detail visualize

--- NASR/Recasting_ver/modules/mix_op.py
+++ NASR/Recasting_ver/modules/mix_op.py
@@ -110,11 +110,11 @@ class MixedEdge(MyModule):
 
     def forward(self, x):
         # print(self.AP_path_alpha)
-        print(self.AP_path_wb)
+        # print(self.AP_path_wb)
         # print(self.active_index)
         # print(self.inactive_index)
         # print(type(self.active_op))
-        if MixedEdge.MODE == 'full' or MixedEdge.MODE == 'two':
+        if MixedEdge.MODE == ' full' or MixedEdge.MODE == 'two':
             output = 0
             for _i in self.active_index:
                 oi = self.candidate_ops[_i](x)
