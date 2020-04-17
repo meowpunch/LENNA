d79c2d138aded1e5220f2f29bd2bd8e65ffff5d2
meowpunch <jinhoonbae@gmail.com>
2020-04-17 14:38:35 +0900

    MEOW: autugrad vs torchprof vs time

--- NASR/analysis/ops_latency.py
+++ NASR/analysis/ops_latency.py
@@ -7,7 +7,6 @@ import torchprof
 from model.test_model import MyModel1, MyModel2, MyModel3, Parallel, Reduction
 from util.latency import get_time
 from util.logger import init_logger
-import time
 
 borders = '-' * 30
 
@@ -18,7 +17,7 @@ class OpsAnalyzer:
         self.counts = counts
         # batch_size(32 or 64) X depth X width X height
         self.X = (torch.rand(size=size).uniform_() > 0.8).float()
-        self.models = [MyModel1(), MyModel3()]  # , Reduction()]
+        self.models = [MyModel1()] #  MyModel3()]  # , Reduction()]
 
     def execute(self):
         return self.process()
@@ -39,8 +38,8 @@ class OpsAnalyzer:
         rows = []
         rows0 = []
         for i in range(self.counts):
-            # with torchprof.Profile(model, use_cuda=True) as prof:
-            #     model(self.X)
+            with torchprof.Profile(model, use_cuda=True) as prof:
+                model(self.X)
             # print(prof.display(show_events=False))
             # def get_latency(target, profiler):
             #     return get_time(prof=profiler, target=target)[0]
@@ -49,6 +48,7 @@ class OpsAnalyzer:
             #     list(map(
             #         functools.partial(get_latency, profiler=prof), m_list
             #     )))
+            rows0.append(sum(get_time(prof=prof, target=model.__class__.__name__)))
 
             with torch.autograd.profiler.profile() as prof:
                 model(self.X)
@@ -60,7 +60,7 @@ class OpsAnalyzer:
         # 'torch.autograd.Profile'
         m_df = pd.DataFrame(rows, columns=["autograd_total_time"])
         # 'torchprof'
-        # m0_df = pd.DataFrame(rows0, columns=m_list)
+        m0_df = pd.DataFrame(rows0, columns=["torchprof"])
         # 'time'
         m1_df = pd.DataFrame(
             data=model.latency_list,
@@ -68,11 +68,11 @@ class OpsAnalyzer:
         )
         # pd.concat([m0_df, m1_df], axis=1)
         # pd.concat([m_df, m1_df], axis=1)
-        return m1_df
+        return pd.concat([m_df, m1_df, m0_df], axis=1)
 
 
 def main():
-    df_list = OpsAnalyzer(counts=1000, size=1024).process()
+    df_list = OpsAnalyzer(counts=10, size=(32, 1, 32, 32)).process()
     print(df_list)
 
 
