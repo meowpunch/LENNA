69d166afb4414fb7c2cc59356d157531359bde96
meowpunch <jinhoonbae@gmail.com>
2020-04-18 15:35:05 +0900

    MEOW: solve error 'outlier.py;

--- NASR/util/outlier.py
+++ NASR/util/outlier.py
@@ -3,8 +3,8 @@ def cut_outlier(df, min_border=None, max_border=None):
         raise ValueError
 
     if min_border is None:
-        return df[df.apply(lambda x: x < x.quantile(max_border), axis=0)]
+        return df.quantile([min_border, 1])
     elif max_border is None:
-        return df[df.apply(lambda x: x.quantile(min_border) < x, axis=0)]
+        return df.quantile([0, max_border])
     else:
-        return df[df.apply(lambda x: (x.quantile(min_border) < x) and (x < x.quantile(max_border)), axis=0)]
+        return df.quantile([min_border, max_border])
