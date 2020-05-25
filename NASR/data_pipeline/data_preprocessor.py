202c11fb7daa12a2cc4e0c1d3c56bf6cee153884
Pyossyoung <pyow14198@gmail.com>
2020-05-25 14:08:03 +0900

    data_preprocessor.. thinking...

--- NASR/data_pipeline/data_preprocessor.py
+++ NASR/data_pipeline/data_preprocessor.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer, make_column_transformer
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 
 class PreProcessor:
     def __init__(self):
@@ -18,7 +18,7 @@ class PreProcessor:
 
     def build_dataset(self):
         """
-            load dataset and split dataset
+        load dataset and split dataset
         :return: train Xy, test Xy
         """
         # preprocessing
@@ -31,12 +31,16 @@ class PreProcessor:
         return train_x, train_y, test_x, test_y
 
     def preprocess(self):
+        '''
+        preprocessing!
+        :return standard->minmax input channel, robust->minmax prob, minmax latency
+        '''
         material = self.dataset
         prob = material.columns.difference(['b_type_0', 'b_type_1', 'latency', 'in_ch'])
         preprocess = make_column_transformer(
-            (MinMaxScaler(), ['in_ch']),
-            (StandardScaler(), prob),
-            (make_pipeline(StandardScaler(), MinMaxScaler()), ['latency']),
+            (make_pipeline(StandardScaler(), MinMaxScaler()), ['in_ch']),
+            (make_pipeline(RobustScaler(), MinMaxScaler()), prob),
+            (MinMaxScaler(), ['latency']),
         )
         fitted = pd.DataFrame(preprocess.fit_transform(material), columns=material.columns[0:167])
         fitted = pd.concat([fitted, material.iloc[:, 167:169]], axis=1)
