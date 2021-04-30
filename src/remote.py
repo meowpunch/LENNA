872123e35cd0aaaa22bb73b8198af32646cf261c
meowpunch <jinhoonbae@gmail.com>
2021-04-30 17:43:05 +0900

    JB: delete all outputs of jupyternotebooks

--- src/remote.py
+++ src/remote.py
@@ -4,9 +4,6 @@ from data_pipeline.main import parallel, single
 
 
 def generate_data(argv):
-    print(argv)
-    print(len(argv))
-
     if len(argv) is 1:
         destination = "training_data/training_data"
     else:
@@ -16,12 +13,8 @@ def generate_data(argv):
     # parallel(destination=destination, outer_loop=250, inner_loop=10, p_num=4)
     single(destination=destination, o_loop=250, i_loop=10)
 
-def train_model(argv):
-    pass
-
 def main(argv):
-    # generate_data(argv)
-    train_model
+    generate_data(argv)
 
 if __name__ == '__main__':
     main(sys.argv)
